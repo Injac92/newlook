366dd673c87b6aa43d3cefe6f4f839a55b755f2e
Injac92 <injacigor92@gmail.com>
2020-07-14 22:32:10 +0200

    Updates

---
+++ precache-manifest.65657316639399abae235fcbe04eacfd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29038877c75fcdb504e9c83882f41af",
+    "url": "/new-look/index.html"
+  },
+  {
+    "revision": "44ded0164e4b2269a3c1",
+    "url": "/new-look/static/css/main.48f324ae.chunk.css"
+  },
+  {
+    "revision": "3eac358106a3cc468c82",
+    "url": "/new-look/static/js/2.567cc440.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/new-look/static/js/2.567cc440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ded0164e4b2269a3c1",
+    "url": "/new-look/static/js/main.920d49b6.chunk.js"
+  },
+  {
+    "revision": "b6b493e6292add4a2240",
+    "url": "/new-look/static/js/runtime-main.7e747f1a.js"
+  },
+  {
+    "revision": "2ada40602b4de39319d03ff2c54264b3",
+    "url": "/new-look/static/media/logo.2ada4060.png"
+  },
+  {
+    "revision": "d5e4e86c36b2bcea4177a78381499045",
+    "url": "/new-look/static/media/portfolio-cover.d5e4e86c.jpg"
+  },
+  {
+    "revision": "cdb5b47fef1dccd28fe1212156c8eb24",
+    "url": "/new-look/static/media/profile_img.cdb5b47f.jpg"
+  },
+  {
+    "revision": "39f603bd3b674be104c1502fb7f2ac27",
+    "url": "/new-look/static/media/youtube_icon.39f603bd.png"
+  }
+]);
